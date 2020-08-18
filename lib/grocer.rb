02df549b33d11fe6709183676039da9ead6ffe23
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-18 17:33:04 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -10,22 +10,15 @@ end
 
 
 def consolidate_cart(cart)
-con_cart=[]
-cart.each_with_index do |key,index|
-  if con_cart===[]
-    con_cart.push key
-    con_cart[0][:count]=1
+ccart=[]
+i=0
+cart.each do |key|
+  if ccart===[]
+    ccart.push key
+    ccart[0][:count]=1
   end
-  if con_cart[index]===cart[index]
-      con_cart.each_with_index do |ckey,cindex|
-        if ckey[:item]===con_cart[index-1][:item]
-          con_cart[cindex][:count]+=1
-        end
-      end
-    else
-        con_cart.push key
-        con_cart[index-1][:count]=1
-    end
+  ccart.each do |ckey|
+    binding.pry
+  end
 end
-return con_cart
 end
